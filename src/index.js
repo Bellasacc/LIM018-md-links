bed2a25c439df95c2ca9ea65cb12545d3c0a4db0
Bellasacc <bellasacc10@gmail.com>
2022-08-23 21:32:36 -0500

    Funcion para obtener la extension de un archivo #3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const fs = require('node:fs');
+const path = require('node:path');
 
 const existsPath = (route) => fs.existsSync(route);
 
-console.log(existsPath('prueba.md'));
-console.log(existsPath('D:\\Laboratoria\\LIM018-md-links\\prueba.md'));
+const extNameFile = (route) => path.extname(route);
+
+console.log(extNameFile('prueba.md'));
+console.log(extNameFile('D:\\Laboratoria\\LIM018-md-links\\prueba.md'));
+console.log(extNameFile('../prueba/ejemplo.txt'));
 
 module.exports = {
   existsPath,
+  extNameFile,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,6 +1,6 @@
 /* const mdLinks = require('../src/index'); */
 
-const { existsPath } = require('../src/index');
+const { existsPath, extNameFile } = require('../src/index');
 
 describe('existsPath', () => {
   it('Para la ruta prueba.md deberia retornar true', () => {
@@ -11,6 +11,17 @@ describe('existsPath', () => {
   });
   it('Para la ruta D:\\Laboratoria\\LIM018-md-links\\gitprueba.md deberia retornar false', () => {
     expect(existsPath('D:\\Laboratoria\\LIM018-md-links\\gitprueba.md')).toBe(false);
+  });
+});
+describe('extNameFile', () => {
+  it('Para la ruta prueba.md deberia retornar .md', () => {
+    expect(extNameFile('prueba.md')).toBe('.md');
+  });
+  it('Para la ruta D:\\Laboratoria\\LIM018-md-links\\prueba.md deberia retornar .md', () => {
+    expect(extNameFile('D:\\Laboratoria\\LIM018-md-links\\prueba.md')).toBe('.md');
+  });
+  it('Para la ruta ../prueba/ejemplo.txt deberia retornar .txt', () => {
+    expect(extNameFile('../prueba/ejemplo.txt')).toBe('.txt');
   });
 });
 
