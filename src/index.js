0e1779a0eaa4fa0e8e0975dce68f55084c41b4f6
Bellasacc <bellasacc10@gmail.com>
2022-08-24 11:08:19 -0500

    obteniendo links en un objeto con propiedades href, text, file #1

--- src/index.js
+++ src/index.js
@@ -8,9 +8,19 @@ const extNameFile = (route) => path.extname(route);
 const readFileMd = (file) => {
   const readFile = fs.readFileSync(file, 'utf-8');
   const exp = /\[(.*?)\]\(.*?\)/gm;
-  const dataFile = readFile.match(exp);
-
-  return dataFile;
+  let dataFile = readFile.match(exp);
+  if (dataFile !== null) {
+    dataFile = dataFile.map((link) => {
+      const finalText = link.indexOf(']');
+      return {
+        href: link.slice(finalText + 2, link.length - 1),
+        text: link.slice(1, finalText),
+        file,
+      };
+    });
+    return dataFile.filter((data) => data.href.startsWith('http') || data.href.startsWith('www'));
+  }
+  return 'no se encontro links';
 };
 
 console.log(readFileMd('prueba.md'));
