6641d7f17f47ffa76bd95515a0e9f1078523ae83
Bellasacc <bellasacc10@gmail.com>
2022-09-04 11:56:14 -0500

    test con options validate #14

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -257,7 +257,7 @@ describe('calculateStats', () => {
         message: 'fail',
       },
     ];
-    expect(calculateStats(arrayLinks)).toEqual({ total: 6, unique: 5, broquen: 3 });
+    expect(calculateStats(arrayLinks)).toEqual({ total: 6, unique: 5 });
   });
 });
 describe('mdLinks', () => {
@@ -309,56 +309,5 @@ describe('mdLinks', () => {
       .then((response) => {
         expect(response).toEqual(arrayLinks);
       });
-  });
-  it('Para una ruta ./prueba/prueba.md y opcion --validate deberia retornar un array de links con status y message', () => {
-    /*  const arrayLinks = [
-      {
-        href: 'https://es.wikipedia.org/wiki/Markdown',
-        text: 'Markdown',
-        file: './prueba/prueba.md',
-        status: 200,
-        message: 'OK',
-      },
-      {
-        href: 'https://nodejs.o/',
-        text: 'Node.js',
-        file: './prueba/prueba.md',
-        status: -3008,
-        message: 'fail',
-      },
-      {
-        href: 'https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg',
-        text: 'md-links',
-        file: './prueba/prueba.md',
-        status: 200,
-        message: 'OK',
-      },
-      {
-        href: 'https://www.google.com',
-        text: 'google',
-        file: './prueba/prueba.md',
-        status: 200,
-        message: 'OK',
-      },
-      {
-        href: 'https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/Functions',
-        text: 'Funciones — bloques de código reutilizables - MDN',
-        file: './prueba/prueba.md',
-        status: 404,
-        message: 'fail',
-      },
-      {
-        href: 'https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/Functions',
-        text: 'Funciones — bloques de código reutilizables - MDN',
-        file: './prueba/prueba.md',
-        status: 404,
-        message: 'fail',
-      },
-    ]; */
-    // options.validate = true;
-    /* mdLinks('./prueba/prueba.md', options)
-      .then((response) => {
-        expect(response).toEqual(arrayLinks);
-      }); */
   });
 });
